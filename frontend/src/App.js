bb203b11de25a1e2e251fcc4f717eca7fd62dee8
cactus1386 <cactusinjast@gmail.com>
2024-07-14 00:58:53 -0700

    Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Card from './Components/Slider';
 import FlipBox from './Components/Card'
+import CustomNavbar from './Components/Navbar';
 
 const App = () => {
 
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Card />} />
-        <Route path='/flip' element={<FlipBox />} />
-      </Routes>
-    </Router>
+    <>
+      <CustomNavbar />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Card />} />
+          <Route path='/flip' element={<FlipBox />} />
+        </Routes>
+      </Router>
+    </>
   );
 };
 
---
+++ frontend/src/Components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../CSS/Navbar.css';
+import img from '../Assets/IDA.png'
+
+const CustomNavbar = () => {
+    return (
+        <Navbar expand="lg" className="custom-navbar">
+            <Navbar.Brand href="/">
+                <img
+                    src={img}
+                    height="60"
+                    alt="Logo"
+                />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
+                <Nav className="nav-links">
+                    <Nav.Link href="/">خانه</Nav.Link>
+                    <Nav.Link href="/products">محصولات</Nav.Link>
+                    <Nav.Link href="/services">سرویس ها</Nav.Link>
+                    <Nav.Link href="/about">درباره ما</Nav.Link>
+                    <Nav.Link href="/contact">تماس با ما</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
+
+export default CustomNavbar;
