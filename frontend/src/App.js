f4064d7443992925001b77a20b4c8dd89ae6d8ce
cactus1386 <cactusinjast@gmail.com>
2024-07-13 23:54:06 -0700

    routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Card from './Components/Slider';
-
+import FlipBox from './Components/Card'
 
 const App = () => {
-  
-  return (
-    <><div class="p-3">
-      <Card/>      
-    </div>
 
-    </>
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Card />} />
+        <Route path='/flip' element={<FlipBox />} />
+      </Routes>
+    </Router>
   );
 };
 
