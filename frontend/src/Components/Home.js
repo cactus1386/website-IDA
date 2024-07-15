438267db818fe60569cc955c2ecefba4102e2ef6
cactus1386 <cactusinjast@gmail.com>
2024-07-15 16:24:32 -0700

    home

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -6,65 +6,65 @@ import '../CSS/Animation.css';
 
 
 const Home = () => {
-    const [isVisible1, setIsVisible1] = useState(false);
-    const [isVisible2, setIsVisible2] = useState(false);
-    const [isVisible3, setIsVisible3] = useState(false);
+  const [isVisible1, setIsVisible1] = useState(false);
+  const [isVisible2, setIsVisible2] = useState(false);
+  const [isVisible3, setIsVisible3] = useState(false);
 
-    useEffect(() => {
-      window.addEventListener('scroll', handleScroll1);
-      return () => {
-        window.removeEventListener('scroll', handleScroll1);
-      };
-    }, []);
-  
-    const handleScroll1 = () => {
-      if (window.scrollY > 0) { // Change this value to the position where you want the animation to trigger
-        setIsVisible1(true);
-      }
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll1);
+    return () => {
+      window.removeEventListener('scroll', handleScroll1);
     };
+  }, []);
+
+  const handleScroll1 = () => {
+    if (window.scrollY > 0) { // Change this value to the position where you want the animation to trigger
+      setIsVisible1(true);
+    }
+  };
 
 
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll2);
-        return () => {
-          window.removeEventListener('scroll', handleScroll2);
-        };
-      }, []);
-    
-      const handleScroll2 = () => {
-        if (window.scrollY > 300) { // Change this value to the position where you want the animation to trigger
-          setIsVisible2(true);
-        }
-      };
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll2);
+    return () => {
+      window.removeEventListener('scroll', handleScroll2);
+    };
+  }, []);
+
+  const handleScroll2 = () => {
+    if (window.scrollY > 300) { // Change this value to the position where you want the animation to trigger
+      setIsVisible2(true);
+    }
+  };
 
 
 
-      useEffect(() => {
-        window.addEventListener('scroll', handleScroll3);
-        return () => {
-          window.removeEventListener('scroll', handleScroll3);
-        };
-      }, []);
-    
-      const handleScroll3 = () => {
-        if (window.scrollY > 700) { // Change this value to the position where you want the animation to trigger
-          setIsVisible3(true);
-        }
-      };
-    return (
-        <div className="container-fluid">
-            <div className={`slide-in ${isVisible1 ? 'visible' : ''}`}>
-                <Slider />
-            </div>
-            <div className={`slide-intori ${isVisible2 ? 'visible' : ''}`}>
-                <Product />
-            </div>
-            <div className={`slide-in ${isVisible3 ? 'visible' : ''}`}>
-                <div class="pt-3"><Application/></div>
-            </div>
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll3);
+    return () => {
+      window.removeEventListener('scroll', handleScroll3);
+    };
+  }, []);
 
-        </div>
-    );
+  const handleScroll3 = () => {
+    if (window.scrollY > 700) { // Change this value to the position where you want the animation to trigger
+      setIsVisible3(true);
+    }
+  };
+  return (
+    <div className="container-fluid">
+      <div>
+        <Slider />
+      </div>
+      <div className={`slide-intori ${isVisible2 ? 'visible' : ''}`}>
+        <Product />
+      </div>
+      <div className={`slide-in ${isVisible3 ? 'visible' : ''}`}>
+        <div class="pt-3"><Application /></div>
+      </div>
+
+    </div>
+  );
 };
 
 export default Home;
