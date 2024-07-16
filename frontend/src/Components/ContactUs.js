a73c9bf76cd3f6f48c40822e9929b9bb84b06a8f
ARS6666 <arsprogramming123@gmail.com>
2024-07-15 22:09:12 -0700

    ContactUs responsive

--- frontend/src/Components/ContactUs.js
+++ frontend/src/Components/ContactUs.js
@@ -3,47 +3,39 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../CSS/General.css';
 
 function ContactUs() {
-  return (<> <div class="pt-3">  
-            <div class="col-md-12 bg-blue fontv" style={{ height: '100px' }}>
-                <div class="d-flex justify-content-center pt-4">
-                    <h2 class="text-light fontv">تمـــاس بـــا مـــا</h2>
-                </div>
+  return (<> <div className="container">
+      <div className="row">
+        <div className="col-12 bg-blue fontv" style={{ height: '100px' }}>
+          <div className="d-flex justify-content-center pt-4">
+            <h2 className="text-light fontv">تمـــاس بـــا مـــا</h2>
+          </div>
+        </div>
+      </div>
+      <div className="row pt-5 fontv">
+        <div class="col-md-2"></div>
+        <div className="col-lg-8 offset-lg-2 col-md-10 offset-md-1 col-sm-12">
+          <div className="card p-3 shadow">
+            <label className="h5">*نام و نام خانوادگی:</label>
+            <input className="form-control input-lg bg-light" type="text" />
+            <label className="h5 pt-3">نام شرکت:</label>
+            <input className="form-control input-lg bg-light" type="text" />
+            <label className="h5 pt-3">ایمیل:</label>
+            <input className="form-control input-lg bg-light" type="text" />
+            <label className="h5 pt-3">*شماره تلفن:</label>
+            <input className="form-control input-lg bg-light" type="text" />
+            <label className="h5 pt-3">متن پیام</label>
+            <textarea className="form-control input-lg bg-light" type="text" />
+            <div className="pt-3">
+              <label className="pb-1">پیوست</label>
+              <input className="form-control input-lg bg-light" type="file" />
             </div>
+            <div className="pt-4 pb-3 d-flex justify-content-center">
+              <button className="btn btn-outline-success col-md-8">ارسال</button>
             </div>
-            <div class="row pt-5 fontv " style={{ paddingRight: '200px' ,paddingLeft :'200px'}}>
-                <div class="row card p-3 shadow">
-                    <label class="h5">*نام و نام خانوادگی:</label>
-                    <div class="col-md-11">
-                        <input class="form-control input-lg bg-light " type="text"/>
-                    </div>
-                    <label class="h5 pt-3">نام شرکت:</label>
-                    <div class="col-md-11">
-                        <input class="form-control input-lg bg-light " type="text"/>
-                    </div>
-                    <label class="h5 pt-3">ایمیل:</label>
-                    <div class="col-md-11">
-                        <input class="form-control input-lg bg-light " type="text"/>
-                    </div>
-                    <label class="h5 pt-3">*شماره تلفن:</label>
-                    <div class="col-md-11">
-                        <input class="form-control input-lg bg-light " type="text"/>
-                    </div>
-                    <label class="h5 pt-3">متن پیام</label>
-                    <div class="col-md-11">
-                        <textarea class="form-control input-lg bg-light " type="text"/>
-                    </div>
-                    <div class="col-md-11 pt-3">
-                        <label class=" pb-1">پیوست</label>
-                    <input class="form-control input-lg bg-light " type="file"/>
-                    </div>
-                    <div class="pt-4 pb-3 d-flex justify-content-center">
-                        <button class="btn btn-outline-success  col-md-8">ارسال</button>
-                    </div>
-                </div>
-
-
-
-            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   
   
   
