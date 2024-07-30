e373c7da2e80f3996ad6f58c3c9c25cf7e54d296
ARS6666 <arsprogramming123@gmail.com>
2024-07-29 23:01:02 -0700

    slider connected

--- frontend/src/Components/Home/Slider.js
+++ frontend/src/Components/Home/Slider.js
@@ -1,23 +1,33 @@
 import { Carousel } from 'react-bootstrap';
 import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
-import img1 from '../../Assets/7.jpg'
-import img2 from '../../Assets/6.jpg'
 import "../../CSS/Slider.css";
 import "../../font/font.css";
 
 
 
 function Slider() {
-    const [Images,setImages] = useState([
-        {Img:img1},
-        {Img:img2},
-    ])
+    const [Images,setImages] = useState([])
     const [isFlipped, setIsFlipped] = useState(false);
 
     const handleFlip = () => {
         setIsFlipped(!isFlipped);
     };
+    const myHeaders = new Headers();
+        myHeaders.append("accept", "application/json");
+        myHeaders.append("authorization", "Basic YWRtaW5AYWRtaW4uY29tOjEyMw==");
+        myHeaders.append("X-CSRFToken", "uRMtwzazkgU3EARvj9tuZwHJUY7L7usZAMA0suaHj3GOrDrPJ2xU4iwenpSCPprj");
+
+    const requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow"
+        };
+
+    fetch("http://127.0.0.1:8000/slider/slider/", requestOptions)
+        .then((response) => response.json())
+        .then((result) => setImages(result))
+        .catch((error) => console.error(error));
 
     return (
                 <div class='pt-3'>
@@ -42,7 +52,7 @@ function Slider() {
                                 <Carousel>
                                     {Images.map(c=> (
                                     <Carousel.Item>
-                                        <img className="d-block w-100 img" src={c.Img} />
+                                        <img className="d-block w-100 img" src={c.image} />
                                     </Carousel.Item>
                                     ))}
                                 </Carousel>
